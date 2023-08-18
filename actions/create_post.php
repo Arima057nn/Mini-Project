47459b8d65a01d3e9a9ccd88eae6868d2e621188
NGUYEN NGOC ANH 20194476 <nngocanh697@gmail.com>
2023-08-18 11:10:49 +0700

    validate create & update

--- actions/create_post.php
+++ actions/create_post.php
@@ -8,21 +8,20 @@ if (!isset($_SESSION['user_success'])) {
 $postController = new PostController($conn);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['submitted']) && $_POST['submitted'] == 1) {
-        $title = $_POST['title'];
-        $level = $_POST['level'];
-        $experience = $_POST['experience'];
-        $target = $_POST['target'];
-        $salary = $_POST['salary'];
-        $address = $_POST['address'];
-        $phone = $_POST['phone'];
+    if (isset($_POST['submitted']) && $_POST['submitted'] == 1 && preg_match("/^[0-9]{10}$/", trim($_POST['phone'])) && strlen(trim($_POST['phone'])) == 10) {
+        $title = trim($_POST['title']);
+        $level = trim($_POST['level']);
+        $experience = trim($_POST['experience']);
+        $target = trim($_POST['target']);
+        $salary = trim($_POST['salary']);
+        $address = trim($_POST['address']);
+        $phone = trim($_POST['phone']);
         $result = $postController->createPost($title, $level, $experience, $target, $salary, $address, $phone, "US0003");
         // var_dump($result);
-
-        // Sau khi cập nhật, bạn có thể chuyển hướng về trang chính hoặc thực hiện các hành động khác
-        header('Location: http://localhost:80/Mini-Project/views/home.php');
-        exit();
     }
+    // Sau khi cập nhật, bạn có thể chuyển hướng về trang chính hoặc thực hiện các hành động khác
+    header('Location: http://localhost:80/Mini-Project/views/home.php');
+    exit();
 }
 
 
